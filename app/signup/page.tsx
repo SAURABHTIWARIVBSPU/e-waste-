c7cd6697778bb6db2ded13971dd1bc8adf55fdbd
SAURABH TIWARI <115872589+SAURABHTIWARIVBSPU@users.noreply.github.com>
2025-04-24 23:32:29 +0530

    Update page.tsx
    adding the backend api

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -32,7 +32,7 @@ export default function SignupPage() {
     }
 
     try {
-      const response = await fetch("http://localhost:4000/api/v1/auth/signup", {
+      const response = await fetch("https://e-waste-cl3k.onrender.com/api/v1/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
